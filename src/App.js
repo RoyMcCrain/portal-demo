a9bf480af0557c83fd2b8ac10bd33a489b74084a
RoyMcCrain <matsuda.r@kanamekey.com>
2020-10-21 19:18:31 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { useState, useRef } from 'react';
-import { createPortal } from "react-dom"
-import './App.css';
-import { CSSTransition } from 'react-transition-group';
-
+import React, { useState, useRef } from "react";
+import { createPortal } from "react-dom";
+import "./App.css";
+import { CSSTransition } from "react-transition-group";
 
 function Portal({ children }) {
-  return (
-    <>{createPortal(children, document.body)}</>
-  )
+  return <>{createPortal(children, document.body)}</>;
 }
 
 function Modal({ isOpen, onClose }) {
@@ -17,51 +14,58 @@ function Modal({ isOpen, onClose }) {
   // 閉じるとき アニメーション終わった（0.4s）後に非表示にする
   const callBacks = {
     onEnter: () => {
-      console.log("onEnter")
+      console.log("onEnter");
     },
     onEntering: () => {
-      console.log("onEntering")
+      console.log("onEntering");
     },
     onEntered: () => {
-      console.log("onEntered")
+      console.log("onEntered");
     },
     onExit: () => {
-      console.log("onExit")
+      console.log("onExit");
     },
     onExiting: () => {
-      console.log("onExitiNg")
+      console.log("onExitiNg");
     },
     onExited: () => {
-      console.log("onExited")
+      console.log("onExited");
     },
   };
   return (
     <Portal>
-      <CSSTransition classNames="modal" in={isOpen} timeout={{ enter: 1000, exit: 1000 }}  {...callBacks} nodeRef={ref} unmountOnExit>
-        <>
-          <div className="modal-base" onClick={onClose}>
-            <div className="modal-content" ref={ref}>
-              <p>Modalです</p>
-            </div>
+      <CSSTransition
+        classNames="modal"
+        in={isOpen}
+        timeout={1000}
+        {...callBacks}
+        nodeRef={ref}
+        unmountOnExit
+      >
+        <div className="modal-base" onClick={onClose}>
+          <div className="modal-content" ref={ref}>
+            <p>Modalです</p>
           </div>
-        </>
+        </div>
       </CSSTransition>
     </Portal>
-  )
+  );
 }
 
-
-function App() {
-  const [isOpen, setOpen] = useState(false)
+export function App() {
+  const [isOpen, setOpen] = useState(false);
   return (
     <>
       <div className="App">
-        <button onClick={() => { setOpen(!isOpen) }}>Push</button>
+        <button
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+        >
+          Push
+        </button>
       </div>
       <Modal isOpen={isOpen} onClose={() => setOpen(false)} />
     </>
   );
 }
-
-
-export default App;
